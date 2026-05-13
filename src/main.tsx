e1ec2e368a3fa3d0552d621c9b783dc9001b7f8c
ruabaie <Ivan.BONDARENKO@raiffeisen.ru>
2026-05-13 18:42:44 +0300

    add middleWeight

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/MMAHORIUM">
       <App />
     </BrowserRouter>
   </StrictMode>
