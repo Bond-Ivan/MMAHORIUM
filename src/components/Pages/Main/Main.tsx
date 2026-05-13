cc3696db4f312157b7a7e3b2e03bc161b6c85f7a
ruabaie <Ivan.BONDARENKO@raiffeisen.ru>
2026-05-13 18:46:56 +0300

    add middleWeight

--- src/components/Pages/Main/Main.tsx
+++ src/components/Pages/Main/Main.tsx
@@ -1,7 +1,7 @@
 import MainCardList from "./MainCardList/MainCardList";
 import MainTournament from "./MainTournament/MainTournament";
 import P4PChampions from "./P4PChampions/P4PChampions";
-import styles from './Main.module.css';
+import styles from '../../../App.module.css';
 import Footer from "../../Footer/Footer";
 
 function Main() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./Main.module.css";
+import "./App.module.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
